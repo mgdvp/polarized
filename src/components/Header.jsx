d5baaae7a9d31f11ae15ee8a9dc9089ed2e453bc
mgdvp <mehigamo@gmail.com>
2025-10-18 09:35:30 +0400

    first

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import '../styles/Header.css';
 const Header = ({ user }) => {
   return (
     <header className="app-header">
-      <Link to="/" className="logo"><img src="/polaryzed-high-resolution-logo-transparent (2).png" alt="Logo" /></Link>
+      <Link to="/" className="logo"><img src="/logo.png" alt="Logo" /></Link>
       {user && user.username && (
         <div className="header-user-info">
           <Link to={`/profile/${user.username}`}>@{user.username}</Link>
